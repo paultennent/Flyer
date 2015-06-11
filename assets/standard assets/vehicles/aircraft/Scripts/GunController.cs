abae06fadb0e8671dccc7f52b9c06b82cccb10ed
Paul Tennent <paul.tennent@nottingham.ac.uk>
2015-06-11 09:00:04 +0100

    Commit of initial version.

---
+++ assets/standard assets/vehicles/aircraft/Scripts/GunController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunController : MonoBehaviour {
+
+	public GameObject projectile;
+	public float speed = 2000;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey("space"))
+		{
+			Transform t = GameObject.Find("AircraftJet").transform;
+			Vector3 playerPosition = new Vector3(t.position.x,t.position.y,t.position.z);
+			Instantiate(projectile,playerPosition,t.rotation);
+		}
+	}
+}
